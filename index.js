1c4363a31168b765738e83e773df26544265843d
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-08-29 09:19:50 -0400

    preparing for deployment

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ import { connectWithDB } from "./utils/connectWithDB.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
-
+import path from "path";
 dotenv.config();
-
+const __dirname = path.resolve();
 const port = process.env.PORT || 3000;
 
 app.use(
@@ -19,10 +19,13 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-app.get("*", express.static("client/dist"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(port, async () => {
   console.log(`Listening on port http://localhost:${port}...`);
